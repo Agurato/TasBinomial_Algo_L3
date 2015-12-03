bb3713c9310a1f0835701d152b5f0e4dee2357aa
paul <pmc.valentin@gmail.com>
2015-12-03 18:34:36 +0100

    Leger avancement

--- tasBinomial.c
+++ tasBinomial.c
@@ -4,6 +4,7 @@
 
 ArbreBinomial CreerArbreBinomial(){
     ArbreBinomial A;
+    A = malloc(sizeof(ArbreBinomial));
     A->degre = 0;
     A->cle = 0;
     A->pere = A->pfg = A->pfd = NULL;
@@ -12,17 +13,24 @@ ArbreBinomial CreerArbreBinomial(){
 
 TasBinomial CreerTasBinomial(){
     TasBinomial T;
+    T = malloc(sizeof(TasBinomial));
     T->A = NULL;
     T->suivant = NULL;
     return T;
 }
 
 ArbreBinomial Inserer(ArbreBinomial A, int cle){
-    A->cle = cle;
-    A->degre = 0;
+    ArbreBinomial ArbreDeux = CreerArbreBinomial();
+    ArbreDeux->pere = ArbreDeux->pfd = ArbreDeux->pfg = NULL;
+    ArbreDeux->cle = cle;
+    ArbreDeux->degre = 0;
+
     return A;
 }
 
+/* On lie deux arbres binomiaux
+ * La racine d'un des arbres devient le fils gauche de l'autre arbre
+ */
 ArbreBinomial Lier(ArbreBinomial A, ArbreBinomial B){
     B->pere = A;
     B->pfd = A->pfg;
@@ -31,15 +39,50 @@ ArbreBinomial Lier(ArbreBinomial A, ArbreBinomial B){
     return A;
 }
 
-TasBinomial AjouterCle(TasBinomial, ArbreBinomial){
-
+/* On insère dans le tas l'arbre,
+ * On vérifie que l'arbre à le même degré que le suivant
+ *      Si oui on les lies
+ *      Si non on laisse ainsi
+ * Puis on revérifie les autres
+ */
+TasBinomial AjouterCle(TasBinomial Tas, ArbreBinomial ArbreB){
+    TasDeux = CreerTas();
+    TasDeux->A = ArbreB;
+    TasDeux->suivant = Tas;
+    if(TasDeux->A->degre == TasDeux->suivant->A->degre){
+        /*Ici il faut lier les deux Arbres pour n'en faire qu'un seul
+         */
+        Tas = Lier(TasDeux->A,Tas->A);
+        TasDeux->suivant = NULL;
+        free(TasDeux);
+    }else{
+    }
+    return Tas;
 }
 
-ArbreBinomial ExtraireMin(TasBinomial){
-
+ArbreBinomial ExtraireMin(TasBinomial T){
+    ArbreBinomial B, C;
+    int cleMin = 100000000000;
+    B = T->A;
+    C = T->A;
+    if(T->suivant != NULL){
+        B = ExtraireMin(T->suivant);
+        while(B!=NULL){
+            if(B->cle < min){
+                min = B->cle;
+                C = B;
+            }
+            B = B->fd;
+        }
+    }
+    return C;
 }
 
-TasBinomial Fusionner(TasBinomial, TasBinomial){
+TasBinomial Fusionner(TasBinomial TasUn, TasBinomial TasDeux){
+    TasBinomial T = CreerTasBinomial();
+    while(TasUn != NULL){
+        if(TasUn->A->degre == TasDeux->A->degre){
+            T = AjouterCle
 
 }
 
