cd41cf8824441c666c4033550bdbd8958f1da883
Agurato <vincent1611@hotmail.fr>
2015-12-05 19:42:56 +0100

    Ajout fonction linkTrees

--- vincent.c
+++ vincent.c
@@ -3,6 +3,9 @@
 #include "vincent.h"
 
 int main(int argc, char const *argv[]) {
+	BinomialTree T = insertKey(NULL, 1);
+
+	printf("T->key=%d / T->degree=%d\n", T->key, T->degree);
 
 	return 0;
 }
@@ -29,7 +32,34 @@ BinomialHeap createHeap() {
 }
 
 BinomialTree insertKey(BinomialTree T, int key) {
-	
+	if(T == NULL) {
+		T = createTree();
+		T->key = key;
+		T->degree ++;
+	}
+	else {
+
+	}
 
 	return T;
+}
+
+// Used to link 2 tree with the same degree
+BinomialTree linkTrees(BinomialTree T1, BinomialTree T2) {
+	if(T1->key < T2->key) {
+		T2->rightBrother = T1->leftSon;
+		T2->father = T1;
+		T1->leftSon = T2;
+		T1->degree ++;
+
+		return T1;
+	}
+	else {
+		T1->rightBrother = T2->leftSon;
+		T1->father = T2;
+		T2->leftSon = T1;
+		T2->degree ++;
+
+		return T2;
+	}
 }
--- vincent.h
+++ vincent.h
@@ -8,3 +8,7 @@ typedef struct cell {
 	BinomialTree tree;
 	struct cell *next;
 } *BinomialHeap;
+
+BinomialTree createTree();
+BinomialHeap createHeap();
+BinomialTree insertKey(BinomialTree T, int key);
